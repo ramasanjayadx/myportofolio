21c87a1b58a52a0bd8e175d9feaae664ac59679c
Rama Sanjaya <ramasanjayadx@gmail.com>
2026-09-16 22:24:39 +0700

    feat: Tutorial 3

---
+++ main/forms.py
@@ -0,0 +1,47 @@
+from django.forms import ModelForm, TextInput, Textarea, URLInput
+
+from main.models import Project
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "category",
+            "thumbnail",
+            "project_url",
+        ]
+
+        labels = {
+            "title": "Nama Proyek",
+            "description": "Deskripsi Proyek",
+            "category": "Kategori Proyek",
+            "thumbnail": "URL Gambar Proyek",
+            "project_url": "URL Proyek",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Portfolio Website",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan Proyekmu",
+                    "rows": 3,
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+            "project_url": URLInput(
+                attrs={
+                    "placeholder": "https://github.com/kakBurhan/burhanquestv4",
+                }
+            ),
+        }
--- portofolio/settings.py
+++ portofolio/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rama-sanjaya-myportofolio.pws.cs.ui.ac.id"]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
+CSRF_TRUSTED_ORIGINS = ["https://rama-sanjaya-myportofolio.pws.cs.ui.ac.id/"]
+
 # Application definition
 
 INSTALLED_APPS = [
